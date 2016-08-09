e340e7410b79f71ed487ba24c3b03427ff40c5aa
Ewelinawaw <ewelina_waw@poczta.fm>
2016-08-09 18:40:33 +0200

    Add files via upload

---
+++ ajax1.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Ewelina on 12.07.2016.
+ */
+
+
+
+$(function(){
+
+    var url = 'http://api.icndb.com/jokes/random';
+
+    var button = document.getElementById('get-joke');
+    button.addEventListener('click', function(){
+        getJoke();
+    });
+
+    var paragraph = document.getElementById('joke');
+
+    function getJoke() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', function () {
+            var response = JSON.parse(xhr.response);
+            paragraph.innerText = response.value.joke;
+        });
+        xhr.send();
+
+
+        var response = JSON.parse(xhr.response);
+    }
+
+
+
+});
+
+
